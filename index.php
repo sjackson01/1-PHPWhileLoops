2c6a8cc9892ab7cbeedaae43220c189801a55e5b
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-30 18:18:33 -0500

    Add: While loop examples

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
+/* !While Loops! */
+
+$currentYear = date('Y');
+$year = $currentYear - 100; 
+
+/*------> 
+
+//Loop while year is <= current year
+//Shows 1919 to 2019
+while($year <= $currentYear){
+    echo $year . "<br /> \n";
+    $year++;
+}
+
+
+
+//Use incremented value within while loop
+//Shows 2020 - 2020 
+while($year++ <= $currentYear){
+    echo $year . "<br /> \n";
+  
+
+//Do While loop 
+//Shows 1919 - 2019
+do{
+    echo $year . "<br />\n";
+}while(++$year <=$currentYear);
+
+<------*/  
 
 ?>
